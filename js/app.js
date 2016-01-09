c9e9d476904dbe3f0a79ff8b0593a5d413c4ee63
kratner <keith.ratner@gmail.com>
2016-01-09 13:57:38 -0800

    First commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/*jslint white: true */
+/*global
+  alert, angular
+*/
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+  '$scope',
+  function($scope){
+    "use strict";
+    $scope.test = 'Hello world!';
+  }
+]);
+
+(function(){
+  "use strict";
+  /*
+  function hasGetUserMedia() {
+    return !!(navigator.getUserMedia || 
+      navigator.webkitGetUserMedia || 
+      navigator.mozGetUserMedia || 
+      navigator.msGetUserMedia);
+  }
+  if (hasGetUserMedia) {
+  	alert("getUserMedia is in effect!");
+  }
+  */
+  if (Modernizr.getusermedia){
+  	alert("gUM in effect");
+    //var gUM = Modernizr.prefixed('getUserMedia', navigator);
+    //gUM({video: true}, function(){});
+  }
+}());
